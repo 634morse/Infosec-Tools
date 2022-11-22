645b9d7cb45233ecece7474aac572d24598df536
634morse <78614365+634morse@users.noreply.github.com>
2022-11-22 00:09:09 -0500

    Add files via upload

---
+++ Infosec-Tools.ps1
@@ -0,0 +1,22 @@
+$pshost = Get-Host
+$psWindow = $pshost.UI.RawUI
+$newSize =$psWindow.BufferSize
+$newSize.Height = 4000
+$newSize.Width = 200
+$psWindow.BufferSize = $newSize
+$newSize = $psWindow.WindowSize
+$newSize.Height = 30
+$newSize.Width = 125
+$psWindow.WindowSize= $newSize
+
+$black = "black"
+#Importing Functions
+$I = ls | select name
+Foreach ($F in $I) {
+    $FName = $F.name
+    if ($F -match "Functions") {
+        Import-Module .\$FName
+    } 
+} 
+
+Welcome-Menu
---
+++ Welcome_menu-Functions.ps1
@@ -0,0 +1,33 @@
+function Welcome-Menu {
+    Clear-Host
+    #$Host.UI.RawUI.BackgroundColor = $black
+    #$host.ui.RawUI.ForegroundColor = “darkgreen”
+write-output "#########################################################################################################################"
+Write-Output "##########################################  \\THE INFOSEC TOOL//  #######################################################"
+write-output "#########################################################################################################################"
+#$host.ui.RawUI.ForegroundColor = “Magenta”
+write-output "Author: Corey Patterson"
+Write-output "Version: 0.1"
+write-output "                                       "
+Write-output "##########################################  \\Tool Usage//  #############################################################"
+Write-output "##########################################  This is a 'Jack of all trades tool'.  #######################################"
+write-output "#The Goal is anything we find use running 'by automation or manually', this tools serves as a place to consolidate those#
+#tools into a single package. These can be used for small use cases or running something at scale.                     ##
+#########################################################################################################################"
+#$host.ui.RawUI.ForegroundColor = “Blue”
+
+write-output "                                       "
+write-output "  \\                   [1] Select '1' If you want to Install All prereqs"
+Write-output "    \\                 [2] Select '2' if you want to use Enumeration Tools"
+write-output "      \\                 [3] Select '3' If you want to take a look at the Random Tools //"
+write-output "        \\               [Q] Select 'Q' If you would like to Quite                   //"
+write-output "          \\                                                                       //" 
+write-output "            \\                                                                   //"
+write-output "              \\                                                               //"
+$Option = Read-Host    "      \\               Please Choose an option                     //"      
+Write-Output "                  \\\\\\\\\\\\\\\\\\\\\\\\\\/////////////////////////////////"
+    Switch ($Option) {
+        2 { Enumeration-Menu-1 }
+        Q { Exit }
+    }
+}
