76d45c20941eb796146e8c2b4d8330adf0db7688
copatterson-fnba <105942294+copatterson-fnba@users.noreply.github.com>
2023-01-20 16:03:15 -0500

    adding to building

--- Modules/Building.ps1
+++ Modules/Building.ps1
@@ -132,4 +132,39 @@ Foreach ($_ in $Shares) {
             $global:ParseScan = .\Dependencies\Parse-Nmap.ps1 .\temp\portscan-$date.xml
         }
         $ParseScan
+
+
+
+
+
+
+
+        ###############################################################
+        $AD_OUS = Get-ADOrganizationalUnit -Filter 'Name -like "*"' | select Name, DistinguishedName, ObjectGuid
+
+Foreach ($OU in $AD_OUS) {
+    $ODS = $OU.DistinguishedName
+    $ODN = $OU.Name
+    $ACLS = (Get-Acl -path "AD:\$ODS").Access | Select ActiveDirectoryRights, InheritanceType, IdentityReference, AccessControlType, IsInherited
+    Foreach ($ACL in $ACLS) {
+        If ($ACL.IdentityReference -notmatch "Builtin" -and $ACL.IdentityReference -notlike "CREATOR OWNER" -and $ACL.IdentityReference -notmatch"NT AUTHORITY" ) {
+        $ADR = $ACL.ActiveDirectoryRights
+        $IT = $ACL.InheritanceType
+        $IR = $ACL.IdentityReference
+        $ACT = $ACL.AccessControlType
+        $II = $ACL.IsInherited
+    
+        $Table = New-Object PSObject -Property @{
+            OU_Name             = $ODS
+            OU_Path             = $ODN
+            AD_Rights           = $ADR
+            IdentityReference   = $IR
+            InheritanceType     = $IT
+            IsInherited         = $II
+            AccessControlType   = $ACT
+        }
+        $Table | Select OU_Name, OU_Path, AD_Rights, IdentityReference, InheritanceType, IsInherited, AccessControlType | export-csv c:\temp\AD_OU_Rights_$((Get-Date).ToString('MM-dd-yyyy')).csv -Append -NoTypeInformation
+        }
+    }
+}
     }
