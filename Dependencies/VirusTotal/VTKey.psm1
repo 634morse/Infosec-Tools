9448ae0a67b7216b8172fd3a8772b2070141c192
unknown <634morse@gmail.com>
2023-01-25 22:07:00 -0500

    editing VT key file

--- Dependencies/VirusTotal/VTKey.psm1
+++ Dependencies/VirusTotal/VTKey.psm1
@@ -1,4 +1,7 @@
 ﻿#Update the variable with your virus total key
+#This variable hosts your api key for Virus Total, so the repo will never contain an actual key
+#This file is marked with "git update-index --assume-unchanged Dependencies/VirusTotal/VTKey.psm1"
+#Once Downloaded, just replace the variable with your key
 function Set_VT_API_KEY {
-    $Global:VTKEY = "XXXXXXXXXXXXX"
+    $Global:VTKEY = "XXXXXfdsfdsfsdfdsfdssdf"
     }
